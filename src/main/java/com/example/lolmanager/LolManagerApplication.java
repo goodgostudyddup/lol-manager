5430d398c20301ceba413c3ea7c6431a42c07860
goodgostudyddup <97076932+goodgostudyddup@users.noreply.github.com>
2024-09-25 22:09:40 +0800

    1

---
+++ src/main/java/com/example/lolmanager/LolManagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.lolmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolManagerApplication {
+
+    public static void main(String[] args) {
+        //
+        SpringApplication.run(LolManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lolmanager/LolManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lolmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
